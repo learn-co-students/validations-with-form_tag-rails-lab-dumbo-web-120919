becd84fdc19cc057f42e137ed8fcb1a0ba62c2de
hylobates-lar <alison.v.quaglia@gmail.com>
2020-01-20 18:39:30 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
@@ -10,11 +20,13 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
-
+  
   private
 
   def post_params
